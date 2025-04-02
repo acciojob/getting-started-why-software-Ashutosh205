ae7c8e6f2b5c609bd22c7bb992fc3f4cc0b41204
Ashutosh solanki <113789261+Ashutosh205@users.noreply.github.com>
2025-04-02 21:53:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your code here
+// Select the div element by ID
+const divElement = document.getElementById("text");
+
+// Add text content
+divElement.textContent = "Welcome to the world of Web development";
